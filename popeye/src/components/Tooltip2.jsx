5b4855a790da2c16f780bb3c5079acceaa431372
sunriseateast <roshankadam5228@gmail.com>
2024-10-27 17:48:54 +0530

    add tooltip2

--- popeye/src/components/Tooltip2.jsx
+++ popeye/src/components/Tooltip2.jsx
@@ -1,17 +1,19 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 function Tooltip2({value}){
+    let prevsize=useRef()
     let size={
-        Softwares:'h-[230px] w-[500px] left-0',
-        Reseller: 'h-[200px] w-[300px] left-56',
-        Support:'h-[150px] w-[280px] left-80',
-        Book_a_Demo:'h-[180px] w-[280px] left-1/2'
+        Softwares:'h-[230px] w-[500px] left-32',
+        Reseller: 'h-[200px] w-[300px] left-64',
+        Support:'h-[200px] w-[400px] left-64',
+        Book_a_Demo:'h-[200px] w-[300px] left-96'
     }
 
-    let visible= size[value] || 'scale-95'
+    let visible=size[value]
 
+    console.log(prevsize)
     return(
-        <div className={`absolute translate-y-[80px] ${visible} bg-slate-100 transistion-all ${visible==='scale-95' ? 'duration-75':'duration-200'} rounded-lg`}>{value}</div>
+        <div ref={prevsize} className={`absolute translate-y-[80px] ${visible} bg-slate-100 transistion-all duration-200 rounded-lg ease-in-out`}></div>
     )
 
 }
