011892a7035c12a1ac562a87df2767ef56566d75
sunriseateast <roshankadam5228@gmail.com>
2025-01-09 10:46:30 +0530

    add Book a Demo

--- popeye/src/components/Tooltip2.jsx
+++ popeye/src/components/Tooltip2.jsx
@@ -18,7 +18,7 @@ function Tooltip2({value}){
         Softwares:'h-[230px] w-[600px] -translate-x-[140px] ', 
         Reseller: 'h-[300px] w-[300px] -translate-x-[50px] ', 
         Support:'h-[180px] w-[310px] -translate-x-[50px]',
-        Book_a_Demo:'h-[150px] w-[300px] translate-x-[70px]',
+        Book_a_Demoo:'h-[150px] w-[300px] translate-x-[70px]',
     }
 
     let visible=size[value]
@@ -35,7 +35,7 @@ function Tooltip2({value}){
         Softwares:'translate-x-[230px] w-[15px] h-[15px]',
         Reseller:'translate-x-[185px] w-[15px] h-[15px]',
         Support:'translate-x-[230px] w-[15px] h-[15px]',
-        Book_a_Demo:'translate-x-[250px] w-[15px] h-[15px]'
+        Book_a_Demoo:'translate-x-[250px] w-[15px] h-[15px]'
     }
 
     let arrvisible=arrow[value]
@@ -47,12 +47,12 @@ function Tooltip2({value}){
 
 
     //Bgbox
-    let boxcss=`bg-zinc-400 translate-y-[21px] opacity-20 rounded transition-all ease-in-out duration-500`
+    let boxcss=`-z-1 hover:cursor-pointer bg-zinc-400 translate-y-[21px] opacity-20 rounded transition-all ease-in-out duration-500`
     let box={
         Softwares:<div className={`absolute transform-gpu h-[38px] w-[91px] ${boxcss} -translate-x-[167px]`}></div>,
         Reseller:<div className={`absolute transform-gpu h-[38px] w-[78px] ${boxcss} -translate-x-[61px]`}></div>,
         Support:<div className={`absolute transform-gpu h-[38px] w-[76px] ${boxcss} translate-x-[37px]`}></div>,
-        Book_a_Demo:<div className={`absolute transform-gpu h-[38px] w-[119px] ${boxcss} translate-x-[153px]`}></div>
+        Book_a_Demo:<div className='absolute hover:cursor-pointer h-[38px] w-[119px] animated-gradient-border translate-x-[153px] translate-y-[21px] transition-all ease-in-out duration-500'></div>
     }
 
     let boxvisible=box[value]
@@ -64,7 +64,7 @@ function Tooltip2({value}){
         <div className='flex justify-center rounded-lg h-full'>
                 <div className='overflow-hidden w-full rounded-lg m-[10px]'>
                    <div className='group border border-slate-200 hover:cursor-pointer hover:bg-slate-200 rounded-lg m-[10px] mt-[25px] p-[13px]'>
-                        <div className='flex space-x-[10px]'>
+                        <div className='flex space-x-[10px] transition-all transform-gpu ease-in-out'>
                             <div className="bg-white group-hover:shadow-md rounded-[22px] flex items-center justify-center h-[60px] w-[60px]">
                                 <img src={whatsapp} className="h-[35px] w-[35px]" alt="WhatsApp" />
                             </div>
@@ -82,7 +82,7 @@ function Tooltip2({value}){
 
                 <div className='overflow-hidden w-full rounded-lg m-[10px]'>
                    <div className='group border border-slate-200 hover:cursor-pointer hover:bg-slate-200 rounded-lg m-[10px] mt-[25px] p-[13px]'>
-                        <div className='flex space-x-[10px]'>
+                        <div className='flex space-x-[10px] transition-all transform-gpu ease-in-out'>
                             <div className="bg-white group-hover:shadow-md rounded-[22px] flex items-center justify-center h-[60px] w-[60px]">
                                 <img src={data} className="h-[35px] w-[35px]" alt="data" />
                             </div>
@@ -101,7 +101,7 @@ function Tooltip2({value}){
             <div className='flex flex-col m-[20px] space-y-[20px]'>
 
                 <div className='overflow-hidden flex flex-col space-y-[10px]'>
-                    <div className="flex items-center justify-center">
+                    <div className="flex items-center justify-center transition-all transform-gpu ease-in-out">
                         <div className="bg-white group-hover:shadow-md rounded-[22px] flex items-center justify-center h-[60px] w-[60px]">
                             <img src={user} className="h-[35px] w-[35px]" alt="data" />
                         </div>
@@ -131,16 +131,16 @@ function Tooltip2({value}){
 
         Support:
         <div className='flex justify-center h-full rounded-lg '>
-            <div className='flex items-center justify-center m-[20px] hover:cursor-pointer hover:bg-slate-200 rounded-lg'>
-                <div className='flex m-[7px] overflow-hidden gap-x-[10px]'>
-                    <div className='shrink-0'>
+            <div className='flex items-center justify-center m-[25px] hover:cursor-pointer hover:bg-slate-200 rounded-lg'>
+                <div className='flex m-[5px] overflow-hidden gap-x-[10px]'>
+                    <div className='shrink-0 transition-all transform-gpu ease-in-out'>
                         <div className="bg-white group-hover:shadow-md rounded-[22px] flex items-center justify-center h-[60px] w-[60px]">
                             <img src={chat} className="h-[35px] w-[35px]" alt="chat" />
                         </div>
                     </div>
                     <div className='w-[200px]'>
                         <p className='text-neutral-500'>
-                            Our <span className='text-black font-bold'>Dedicated Support</span> Team is here to ensure a smooth experience
+                            Our <span className='text-black font-bold'>Dedicated Support</span> Team is here to ensure a smooth experience.
                         </p>
                     </div>
                 </div>
