58ae5690dd7734ad80a350e6e20faa9e4c5ea461
sunriseateast <roshankadam5228@gmail.com>
2025-02-24 23:06:47 +0530

    shift windows

--- popeye/src/HeroSection/Herosec.jsx
+++ popeye/src/HeroSection/Herosec.jsx
@@ -1,11 +1,26 @@
-import React,{useRef} from "react";
+import React,{useRef,useEffect} from "react";
 import { gsap } from "gsap";
 import { useGSAP } from '@gsap/react';
 import "./HeroSection.css"
 
+
 function Herosec(){
+
     let button = useRef();
-    const { contextSafe } = useGSAP({scope:button}) 
+    let mask=useRef()
+    const { contextSafe } = useGSAP({scope:button})
+
+
+
+    useEffect(()=>{
+        if(mask.current){
+            let children=Array.from(mask.current.children)
+            children.forEach(child => {
+               
+            });
+        }
+
+    },[]) 
 
         //GSAP animation for softwares
         const buttonanimationEnter = contextSafe(() => {
@@ -47,16 +62,11 @@ function Herosec(){
 
     return(
         <div className="flex flex-col space-y-[50px] items-center justify-center h-full w-full hero">
-            <div className="z-0">
+            <div className="">
                 <p className="text-[#78716C]">MORE THAN PLATFORM</p>
                 <p className="text-[24px] md:text-[100px] font-bold z-10">The Hub of Softwares</p>
-                <div className="absolute flex flex-wrap justify-end space-x-[10px] space-y-[10px] top-0 right-0 h-full w-full overflow-hidden cut">
-                    {Array(200).fill(null).map(() => (
-                        <div className="border border-[#78716C] h-[55px] w-[55px]"></div>
-                    ))}
-                </div>
             </div>
-            <div className='flex items-center justify-center z-10'>
+            <div className='flex items-center justify-center'>
                 <button ref={button} className='border border-[#F5F5F4] rounded-xl p-[15px] w-[170px] h-[60px]' onMouseEnter={buttonanimationEnter} onMouseLeave={buttonanimationExit}>All Softwares</button>
             </div>
         </div>
