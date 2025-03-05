361dbb900c1d6be149b806499af75e30834bde9d
roshan <roshankadam5228@gmail.com>
2025-03-05 20:51:05 +0530

    add hireachy going to change

--- src/HeroSection/Herosec.jsx
+++ src/HeroSection/Herosec.jsx
@@ -5,7 +5,7 @@ import Dots from "../svg/Dots";
 import Check from "../svg/Check";
 import gsap from 'gsap';
 import TextPlugin from 'gsap/TextPlugin';
-import SplitType from 'split-type'
+import Testimonials from "./Testimonials";
 
 gsap.registerPlugin(TextPlugin)
 
@@ -13,38 +13,6 @@ gsap.registerPlugin(TextPlugin)
 function Herosec(){
 
     const flipwds=useRef(null)
-    // const [herowds,setHerowds]=useState([
-    //     <p className="transition-all transform-gpu duration-700 ease-in-out">Softwares</p>,
-    //     <p className="transition-all transform-gpu duration-700 ease-in-out ">Solutions</p>,
-    //     <p className="transition-all transform-gpu duration-700 ease-in-out opacity-0 blur">Trust</p>
-    // ])
-
-    // let [herotxt,setHerotxt]=useState(herowds[0])
-    // let counterRef=useRef(0)
-    // const frameRef=useRef(null)
-    // const lastUpdateTime=useRef(0)
-
-    // useEffect(()=>{
-
-    //     function fliptext(timestamp){
-    //         if(timestamp - lastUpdateTime.current >= 1500){
-    //             if (counterRef.current==3){
-    //                 counterRef.current=0
-    //             }
-    //             setHerotxt(herowds[counterRef.current])
-    //             setHerotxt(prev=>console.log(prev.props.className))
-    //             lastUpdateTime.current=timestamp
-    //             counterRef.current=counterRef.current + 1
-    //         }
-    //         frameRef.current=requestAnimationFrame(fliptext)
-    //     }
-    //     frameRef.current=requestAnimationFrame(fliptext)
-
-    //     return(()=>{
-    //         cancelAnimationFrame(frameRef.current)
-    //     })
-
-    // },[])
 
     useEffect(() => {
 
@@ -58,7 +26,7 @@ function Herosec(){
                 {
                     opacity:1,
                     duration:2,
-                    delay:1,
+                    delay:1.5,
                     text:{
                         value:"Solutions",
                         newClass:"animated-gradient"
@@ -69,10 +37,10 @@ function Herosec(){
     }, []);
 
     return(
-        <div className="fixed grid grid-cols-2 h-full w-full hero bg-fixed">
+        <div className="grid grid-cols-2 h-full w-full hero bg-fixed">
             <div className="flex items-center justify-center">
                 <div className="flex flex-col items-center justify-center pr-[150px]">
-                    <p className="text-[24px] md:text-[70px] font-bold leading-[1.1] text-center">
+                    <p className="text-[24px] md:text-[70px] font-bold leading-[1.0] text-center">
                         The Hub of <span ref={flipwds}>Softwares</span>
                     </p>
                     <div className="flex space-x-[30px] text-[#78716C] mt-[28px]">
@@ -95,37 +63,14 @@ function Herosec(){
                                 <Dots/>
                             </div>
                         </div>
-
-                        {/* <div className="flex items-center justify-center rounded-xl bg-slate-100 w-[200px] border-2 border-slate-200">
-                            <div className="h-[20px] w-[20px] mt-[1px]">
-                                <Check/>
-                            </div>
-                            <p className="ml-[5px] mr-[5px] text-[13px]">BEYOND LIMITS ALWAYS</p>
-                            <div className="h-[10px] w-[10px]">
-                                <Dots/>
-                            </div>
-                        </div> */}
-
-                        {/* <p>BEYOND JUST SOFTWARES</p>
-                        <p>PLATFORM THAT LISTENS</p> */}
                     </div>
                     <button className='tracking-wider mt-[45px] border border-[#F5F5F4] rounded-xl p-[15px] w-[170px] h-[60px] hover:bg-[#F5F5F4] hover:text-black'>Free Trial</button>
                 </div>
             </div>
-            <div className="">
-                hello
+            <div className="mask">
+                <Testimonials/>
             </div>
         </div>
-
-        // <div className="flex flex-col space-y-[50px] items-center justify-center h-full w-full hero">
-        //     <div className="">
-        //         <p className="text-[#78716C]">MORE THAN PLATFORM</p>
-        //         <p className="text-[24px] md:text-[100px] font-bold z-10">The Hub of Softwares</p>
-        //     </div>
-        //     <div className='flex items-center justify-center'>
-        //         <button ref={button} className='border border-[#F5F5F4] rounded-xl p-[15px] w-[170px] h-[60px] hover:bg-[#F5F5F4] hover:text-black'>All Softwares</button>
-        //     </div>
-        // </div>
     )
 }
 
---
+++ src/HeroSection/Testimonials.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+
+function Testimonials(){
+    return(
+        <div className="h-full w-full grid grid-cols-3">
+            <div className="flex justify-center bg-amber-500">
+                Hello
+            </div>
+            <div className="flex justify-center bg-pink-500">
+                Roshan
+            </div>
+            <div className="flex justify-center bg-lime-500">
+                Kadam
+            </div>
+        </div>
+    )
+}
+
+export default Testimonials
